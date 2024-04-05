4cd96928058b24a7d3e9e8ab00903bc346323fa9
Philip Coombes <phil@web2wire.com>
2024-04-05 08:58:37 +0100

    Added some custom Symfony config post-install

--- build/assets/fpm/bootstrap.sh
+++ build/assets/fpm/bootstrap.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+set -e
 export COMPOSER_ALLOW_SUPERUSER=1
 if [ ! -f app/composer.json ]
 then
     composer create-project symfony/skeleton:"7.0.*" app && \
         cd app && \
-        composer require webapp
+        composer require webapp && \
+        cp /tmp/symfony/.env . && \
+        cp /tmp/symfony/doctrine.yaml config/packages/
 fi
 php-fpm
